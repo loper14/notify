9aec520ada49741e2b0e4f68b81705e682a59b6a
loper14 <alisherovhamidullo2@gmail.com>
2023-07-07 14:53:35 +0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
-
 const App = () => {
-  return <div>App</div>;
+  let a = () => {
+    Notification.requestPermission().then((perm) => {
+      if (perm === "granted") {
+        console.log("working");
+        return new Notification("Permission granted");
+      }
+    });
+  };
+  new Notification("Permission granted");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        height: "100vh",
+        alignItems: "center",
+        backgroundColor: "red",
+      }}
+    >
+      <button
+        style={{
+          width: "200px",
+          height: "100px",
+          backgroundColor: "blue",
+        }}
+        onClick={() => a()}
+      >
+        Click
+      </button>
+    </div>
+  );
 };
 
 export default App;
